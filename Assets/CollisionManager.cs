31c5843b637945fb24f696d1dfd8adfd947f2f6d
mems <memsxpice@gmail.com>
2026-09-23 13:40:21 -0600

    El flujo de la interfaz

--- Assets/CollisionManager.cs
+++ Assets/CollisionManager.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class CollisionManager : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D coll){
-        Debug.Log("Toco la moneda!");
+
+    Animator coins_Animator;
+    GameObject coin;
+    GameObject[] coins;
+
+    void Awake(){
+        coins = GameObject.FindGameObjectsWithTag("CoinsDrop");
     }
 
+    void OnTriggerEnter2D(Collider2D coll){
+        Debug.Log("Toco la moneda!");
+
+        foreach(GameObject coin in coins){
+            coins_Animator = coin.GetComponent<Animator>();
+            coins_Animator.SetBool("isDroping", true);
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D coll){
+        Debug.Log("Toco la moneda!");
+
+        foreach(GameObject coin in coins){
+            coins_Animator = coin.GetComponent<Animator>();
+            coins_Animator.SetBool("isDroping", false);
+        }
+
+    }
 }
